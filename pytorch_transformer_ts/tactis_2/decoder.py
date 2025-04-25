98c54f78213cbbd4ac47dc17dcaddb89e380e90a
Juan Boullosa <juan.manuel.boullosa.novo@uni-oldenburg.de>
2025-04-25 23:52:07 +0200

    Possible fix to The size of tensor a (128) must match the size of tensor b (5280) at non-singleton dimension 1

--- pytorch_transformer_ts/tactis_2/decoder.py
+++ pytorch_transformer_ts/tactis_2/decoder.py
@@ -212,7 +212,8 @@ class CopulaDecoder(nn.Module):
         self.copula_loss_val = copula_loss_batch # Store for potential logging
 
         # Return the two loss components per batch item
-        return marginal_logdet_batch, copula_loss_batch
+        # BUGFIX @boujuan possible fix to "The size of tensor a (128) must match the size of tensor b (5280) at non-singleton dimension 1" bug 
+        return marginal_logdet_batch.sum(dim=1), copula_loss_batch
 
 
     def sample(
